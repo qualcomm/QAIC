065bd724cf3ff96b7dbc50e384bfc4ddb6693c1a
Suraj Singh <sursing@qti.qualcomm.com>
2026-02-20 03:00:04 +0530

    Added include_idl_1 test case
    Signed-off-by: Suraj Singh <sursing@qti.qualcomm.com>

---
+++ testcases/include_idl_1/src/include_idl_1_imp.c
@@ -0,0 +1,64 @@
+/*==============================================================================
+  Copyright (c) 2012-2014, 2020 Qualcomm Technologies, Inc.
+  All rights reserved. Qualcomm Proprietary and Confidential.
+==============================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "HAP_farf.h"
+#include "include_idl_1.h"
+
+int include_idl_1_open(const char*uri, remote_handle64* handle) {
+   void *tptr = NULL;
+  /* can be any value or ignored, rpc layer doesn't care
+   * also ok
+   * *handle = 0;
+   * *handle = 0xdeadc0de;
+   */
+   tptr = (void *)malloc(1);
+   *handle = (remote_handle64)tptr;
+   return 0;
+}
+
+/**
+ * @param handle, the value returned by open
+ * @retval, 0 for success, should always succeed
+ */
+int include_idl_1_close(remote_handle64 handle) {
+   if (handle)
+      free((void*)handle);
+   return 0;
+}
+
+int include_idl_1_sum(remote_handle64 h, const int* vec, int vecLen)
+{
+	int sum=0;
+	for(int j=0;j<vecLen;j++)
+	{
+		sum+=vec[j];
+	}
+	if(sum!=105)
+	{
+		FARF(ALWAYS,"Data Mismatch error sum=%d",sum);
+		return -1;
+	}
+  	return 0;
+}
+
+
+int include_idl_1_diff(remote_handle64 h, const int* vec, int vecLen)
+{
+	int diff=0;
+	for(int j=0;j<vecLen;j++)
+	{
+		diff+=vec[j];
+	}
+   diff-=100;
+	if(diff!=5)
+	{
+		FARF(ALWAYS,"Data Mismatch error diff=%d",diff);
+		return -1;
+	}
+  	return 0;
+}
---
+++ testcases/include_idl_1/src/include_idl_1_main.c
@@ -0,0 +1,84 @@
+#include "include_idl_1_test.h"
+#include "rpcmem.h"
+#include "remote.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "AEEStdErr.h"
+
+static void print_usage()
+{
+  printf( "Usage:\n"
+    "    include_idl_1 [-d domain] [-U unsigned_PD] -n array_size\n\n"
+    "Options:\n"
+    "-d domain: Run on a specific domain.\n"
+    "    0: Run the example on ADSP\n"
+    "    3: Run the example on CDSP\n"
+    "    1: Run the example on MDSP\n"
+    "    2: Run the example on SDSP\n"
+    "        Default Value: 3(CDSP) for targets having CDSP and 0(ADSP) for targets not having CDSP like Agatti.\n"
+    "-U unsigned_PD: Run on signed or unsigned PD.\n"
+    "    0: Run on signed PD.\n"
+    "    1: Run on unsigned PD.\n"
+    "        Default Value: 1\n"
+    "-n array_size: Natural number up to which sum is calculated from 0 to (n-1)\n"
+    "        Default Value: 1\n"
+    );
+}
+
+int main(int argc, char* argv[])
+{
+  int nErr = 0;
+  int num = 1;
+  int domain = 0;
+  int requested_pd = 1;
+  bool is_signedpd_requested = false;
+  int option = 0;
+
+  while ((option = getopt(argc, argv,"d:U:n:")) != -1) {
+    switch (option) {
+      case 'd' : domain = atoi(optarg);
+        break;
+      case 'U' : requested_pd = atoi(optarg);
+        break;
+      case 'n' : num = atoi(optarg);
+        break;
+      default:
+        print_usage();
+      return -1;
+  }
+  }
+
+  if (domain < 0 || domain > 3) {
+     nErr = -1;
+     printf("\nInvalid domain %d\n", domain);
+     goto bail;
+  }
+
+  if (requested_pd == 0)
+    is_signedpd_requested = true;
+  else if (requested_pd == 1)
+    is_signedpd_requested = false;
+  else {
+    nErr = AEE_EBADPARM;
+    printf("\nERROR 0x%x: Invalid unsigned PD flag %d\n", nErr, requested_pd);
+    print_usage();
+    goto bail;
+  }
+
+
+  printf("\n- Starting include_idl_1 test on domain %d\n", domain);
+  nErr = include_idl_1_test(domain, num, is_signedpd_requested);
+
+bail:
+  if (nErr) {
+    printf("- include_idl_1 example failed with nErr = %d\n\n", nErr);
+  } else {
+    printf("- success\n\n");
+  }
+
+  return nErr;
+}
