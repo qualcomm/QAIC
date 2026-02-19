eae90ca4b56349e56af2cc3239808dd0efc0a34e
Suraj Singh <sursing@qti.qualcomm.com>
2026-02-19 14:28:11 +0530

    Added array test case
    Signed-off-by: Suraj Singh <sursing@qti.qualcomm.com>

---
+++ testcases/array/src/array_imp.c
@@ -0,0 +1,200 @@
+/*==============================================================================
+  Copyright (c) 2012-2014, 2020 Qualcomm Technologies, Inc.
+  All rights reserved. Qualcomm Proprietary and Confidential.
+==============================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "HAP_farf.h"
+#include "array.h"
+
+int array_open(const char*uri, remote_handle64* handle) {
+   void *tptr = NULL;
+  /* can be any value or ignored, rpc layer doesn't care
+   * also ok
+   * *handle = 0;
+   * *handle = 0xdeadc0de;
+   */
+   tptr = (void *)malloc(1);
+   *handle = (remote_handle64)tptr;
+   //assert(*handle);
+   return 0;
+}
+
+/**
+ * @param handle, the value returned by open
+ * @retval, 0 for success, should always succeed
+ */
+int array_close(remote_handle64 handle) {
+   if (handle)
+      free((void*)handle);
+   return 0;
+}
+
+int fact(int a){
+
+  if(a == 0||a == 1)
+    return 1;
+  else
+    return a*fact(a-1);
+}
+
+int array_sum(remote_handle64 h, const alpha* vec, int64_t* res, int resLen)
+{
+  FARF(ALWAYS, "reached imp execution");
+  if(resLen!=29){
+    FARF(ERROR, "=== Data Mismatch ===\n");
+    return -1;
+  }
+  for(int i=0;i<10;i++){
+
+    if((vec->number)[i]!=i || (int)(vec->num2)[i]!=i || (int)(vec->num3)[i]!=i ||(int)(vec->num4)[i]!=i ||(vec->num5)[i]!=i ||(vec->num6)[i]!=i ||(vec->num7)[i]!=i ||(vec->num8)[i]!=i ||(vec->num9)[i]!=i ||(vec->arr)[i]!=i ||(vec->arr1)[i]!=i ||(vec->arr2)[i]!=i ||(vec->arr3)[i]!=i ||(vec->arr4)[i]!=i ||(vec->arr5)[i]!=i ||(vec->arr6)[i]!=i ||(vec->arr7)[i]!=i ||(vec->arr8)[i]!=i ||(vec->arr9)[i]!=i ||(vec->var)[i]!=i ||(vec->var1)[i]!=i ||(vec->var2)[i]!=i ||(vec->var3)[i]!=i ||(vec->var4)[i]!=i ||(vec->var5)[i]!=i ||(vec->var6)[i]!=i ||(vec->var7)[i]!=i ||(vec->var8)[i]!=i%2 || (vec->enum1)[i]!=i%3){
+      FARF(ERROR, "=== Data Mismatch ===\n");
+      return -1;
+    }
+  }
+  for(int i=0;i<resLen;i++){
+    res[i] = 0;
+  }
+  for(int i=0;i<10;i++){
+    res[0] = res[0] + (vec->number)[i];
+    res[1] = res[1] + (int)(vec->num2)[i];
+    res[2] = res[2] + (int)(vec->num3)[i];
+    res[3] = res[3] + (int)(vec->num4)[i];
+    res[4] = res[4] + (vec->num5)[i];
+    res[5] = res[5] + (vec->num6)[i];
+    res[6] = res[6] + (vec->num7)[i];
+    res[7] = res[7] + (vec->num8)[i];
+    res[8] = res[8] + (vec->num9)[i];
+    res[9] = res[9] + (vec->arr)[i];
+    res[10] = res[10] + (vec->arr1)[i];
+    res[11] = res[11] + (vec->arr2)[i];
+    res[12] = res[12] + (vec->arr3)[i];
+    res[13] = res[13] + (vec->arr4)[i];
+    res[14] = res[14] + (vec->arr5)[i];
+    res[15] = res[15] + (vec->arr6)[i];
+    res[16] = res[16] + (vec->arr7)[i];
+    res[17] = res[17] + (vec->arr8)[i];
+    res[18] = res[18] + (vec->arr9)[i];
+    res[19] = res[19] + (vec->var)[i];
+    res[20] = res[20] + (vec->var1)[i];
+    res[21] = res[21] + (vec->var2)[i];
+    res[22] = res[22] + (vec->var3)[i];
+    res[23] = res[23] + (vec->var4)[i];
+    res[24] = res[24] + (vec->var5)[i];
+    res[25] = res[25] + (vec->var6)[i];
+    res[26] = res[26] + (vec->var7)[i];
+    res[27] = res[27] + (vec->var8)[i];
+    res[28] = res[28] + (vec->enum1)[i];
+  }
+  return 0;
+}
+
+int array_method1(remote_handle64 _h, const beta* struc1, beta* struc2, beta* struc3){
+
+  for(int i=0;i<5;i++){
+    if(struc1->a.num[i]!=i || struc3->a.num[i] != i){
+      FARF(ERROR, "=== Data Mismatch ===\n");
+      return -1;
+    }
+  }
+  for(int i=0;i<5;i++){
+    struc2->a.num[i] = fact(struc1->a.num[i]);
+    struc3->a.num[i] = struc3->a.num[i] * struc3->a.num[i];
+  }
+  return 0;
+}
+
+int array_method3(remote_handle64 _h, const beta* seq1, int seq1Len, beta* seq2, int seq2Len, beta* seq3, int seq3Len){
+
+  if(seq1Len != seq2Len || seq1Len != seq3Len){
+    FARF(ERROR, "=== Data Mismatch ===\n");
+    return -1;
+  }
+  for(int i =0 ;i<seq1Len;i++){
+    for(int j=0;j<5;j++){
+      if(seq1[i].a.num[j] != i+j || seq3[i].a.num[j] != i+j){
+        FARF(ERROR, "=== Data Mismatch ===\n");
+        return -1;
+      }
+    }
+  }
+  for(int i =0;i<seq2Len;i++){
+    for(int j=0;j<5;j++){
+      seq2[i].a.num[j] = seq1[i].a.num[j] * seq1[i].a.num[j];
+      seq3[i].a.num[j] = fact(seq3[i].a.num[j]);
+    }
+  }
+  return 0;
+}
+
+int array_method2(remote_handle64 _h, const alpha* seq1, int seq1Len, alpha* seq2, int seq2Len, alpha* seq3, int seq3Len){
+
+  if(seq1Len != seq2Len || seq3Len != seq1Len){
+    FARF(ERROR, "=== Data Mismatch ===\n");
+    return -1;
+  }
+  int sum1 = 0, sum2 = 0;
+  for(int i =0 ;i<seq1Len;i++){
+    for(int j=0;j<10;j++){
+      sum1 = sum1 +seq1[i].number[j] + (int)seq1[i].num2[j] + (int)seq1[i].num3[j] + (int)seq1[i].num4[j] + seq1[i].num5[j] + seq1[i].num6[j] + seq1[i].num7[j] + seq1[i].num8[j] + seq1[i].num9[j] + seq1[i].arr[j] + seq1[i].arr1[j] + seq1[i].arr2[j] + seq1[i].arr3[j] + seq1[i].arr4[j]+ seq1[i].arr5[j]+ seq1[i].arr6[j]+ seq1[i].arr7[j]+ seq1[i].arr8[j]+ seq1[i].arr9[j] + seq1[i].var[j]+ seq1[i].var1[j]+ seq1[i].var2[j]+ seq1[i].var3[j]+ seq1[i].var4[j]+ seq1[i].var5[j]+ seq1[i].var6[j]+ seq1[i].var7[j]+ seq1[i].var8[j] + seq1[i].enum1[j];
+      sum2 = sum2 +seq3[i].number[j] + (int)seq3[i].num2[j] + (int)seq3[i].num3[j] + (int)seq3[i].num4[j] + seq3[i].num5[j] + seq3[i].num6[j] + seq3[i].num7[j] + seq3[i].num8[j] + seq3[i].num9[j] + seq3[i].arr[j] + seq3[i].arr1[j] + seq3[i].arr2[j] + seq3[i].arr3[j] + seq3[i].arr4[j]+ seq3[i].arr5[j]+ seq3[i].arr6[j]+ seq3[i].arr7[j]+ seq3[i].arr8[j]+ seq3[i].arr9[j] + seq3[i].var[j]+ seq3[i].var1[j]+ seq3[i].var2[j]+ seq3[i].var3[j]+ seq3[i].var4[j]+ seq3[i].var5[j]+ seq3[i].var6[j]+ seq3[i].var7[j]+ seq3[i].var8[j] + seq3[i].enum1[j];
+    }
+  }
+  if(sum1 != 2750 || sum2 != 2750){
+    FARF(ERROR, "=== Data Mismatch ===\n");
+    return -1;
+  }
+
+  for(int j =0;j<seq2Len;j++){
+    for(int i=0;i<10;i++){
+      (seq2[j].num2)[i] =(wchar_t) (i+j);
+      (seq2[j].num3)[i] =(unsigned char) (i+j);
+      (seq2[j].num4)[i] = (char) (i+j);
+      (seq2[j].number)[i] = (seq2[j].num5)[i] = (seq2[j].num6)[i] = (seq2[j].num7)[i]= (seq2[j].num8)[i] = (seq2[j].num9)[i] = (seq2[j].arr)[i] = (seq2[j].arr1)[i] = (seq2[j].arr2)[i] = (seq2[j].arr3)[i] = (seq2[j].arr4)[i] = (seq2[j].arr5)[i] = (seq2[j].arr6)[i] = (seq2[j].arr7)[i] = (seq2[j].arr8)[i] = (seq2[j].arr9)[i] = (seq2[j].var)[i] = (seq2[j].var1)[i] = (seq2[j].var2)[i] = (seq2[j].var3)[i] = (seq2[j].var4)[i] = (seq2[j].var5)[i] = (seq2[j].var6)[i] = (seq2[j].var7)[i] = fact((i+j)%3);
+      (seq2[j].var8)[i] = (i+j)%2;
+      seq2[j].enum1[i] = GREY;
+      (seq3[j].num2)[i] =(wchar_t) (seq3[j].num2[i]);
+      (seq3[j].num3)[i] =(unsigned char) (seq3[j].num3[i]);
+      (seq3[j].num4)[i] = (char) (seq3[j].num4[i]);
+      (seq3[j].number)[i] = (seq3[j].num5)[i] = (seq3[j].num6)[i] = (seq3[j].num7)[i]= (seq3[j].num8)[i] = (seq3[j].num9)[i] = (seq3[j].arr)[i] = (seq3[j].arr1)[i] = (seq3[j].arr2)[i] = (seq3[j].arr3)[i] = (seq3[j].arr4)[i] = (seq3[j].arr5)[i] = (seq3[j].arr6)[i] = (seq3[j].arr7)[i] = (seq3[j].arr8)[i] = (seq3[j].arr9)[i] = (seq3[j].var)[i] = (seq3[j].var1)[i] = (seq3[j].var2)[i] = (seq3[j].var3)[i] = (seq3[j].var4)[i] = (seq3[j].var5)[i] = (seq3[j].var6)[i] = (seq3[j].var7)[i] = fact((seq3[j].number[i])%3);
+      (seq3[j].var8)[i] = (seq3[j].var8[i])%2;
+      seq3[j].enum1[i] = DARK;
+    }
+  }
+  return 0;
+}
+
+int array_sum2(remote_handle64 _h, alpha* vec1, alpha* vec2){
+
+  for(int i=0;i<10;i++){
+
+    if((vec2->number)[i]!=i || (int)(vec2->num2)[i]!=i || (int)(vec2->num3)[i]!=i ||(int)(vec2->num4)[i]!=i ||(vec2->num5)[i]!=i ||(vec2->num6)[i]!=i ||(vec2->num7)[i]!=i ||(vec2->num8)[i]!=i ||(vec2->num9)[i]!=i ||(vec2->arr)[i]!=i ||(vec2->arr1)[i]!=i ||(vec2->arr2)[i]!=i ||(vec2->arr3)[i]!=i ||(vec2->arr4)[i]!=i ||(vec2->arr5)[i]!=i ||(vec2->arr6)[i]!=i ||(vec2->arr7)[i]!=i ||(vec2->arr8)[i]!=i ||(vec2->arr9)[i]!=i ||(vec2->var)[i]!=i ||(vec2->var1)[i]!=i ||(vec2->var2)[i]!=i ||(vec2->var3)[i]!=i ||(vec2->var4)[i]!=i ||(vec2->var5)[i]!=i ||(vec2->var6)[i]!=i ||(vec2->var7)[i]!=i ||(vec2->var8)[i]!=i%2 || (vec2->enum1)[i]!=i%3){
+      FARF(ERROR, "=== Data Mismatch ===\n");
+      return -1;
+    }
+  }
+
+  for(int i=0;i<10;i++){
+    (vec1->num2)[i] =(wchar_t) i;
+    (vec1->num3)[i] =(unsigned char) i;
+    (vec1->num4)[i] = (char) i;
+    (vec1->number)[i] = (vec1->num5)[i] = (vec1->num6)[i] = (vec1->num7)[i]= (vec1->num8)[i] = (vec1->num9)[i] = (vec1->arr)[i] = (vec1->arr1)[i] = (vec1->arr2)[i] = (vec1->arr3)[i] = (vec1->arr4)[i] = (vec1->arr5)[i] = (vec1->arr6)[i] = (vec1->arr7)[i] = (vec1->arr8)[i] = (vec1->arr9)[i] = (vec1->var)[i] = (vec1->var1)[i] = (vec1->var2)[i] = (vec1->var3)[i] = (vec1->var4)[i] = (vec1->var5)[i] = (vec1->var6)[i] = (vec1->var7)[i] = fact(i%3);
+    (vec1->var8)[i] = i%2;
+  }
+  vec1->enum1[0] = vec1->enum1[3] = vec1->enum1[6] = vec1->enum1[9] = WHITE;
+  vec1->enum1[1] = vec1->enum1[4] = vec1->enum1[7] = GREY;
+  vec1->enum1[2] = vec1->enum1[5] = vec1->enum1[8] = DARK;
+  for(int i=0;i<10;i++){
+    (vec2->num2)[i] =(wchar_t) ((int) (vec2->num2)[i]+1);
+    (vec2->num3)[i] =(unsigned char) ((int)((vec2->num3)[i]+1));
+    (vec2->num4)[i] = (char) ((int) (vec2->num2)[i]+1);;
+    (vec2->number)[i] = (vec2->num5)[i] = (vec2->num6)[i] = (vec2->num7)[i]= (vec2->num8)[i] = (vec2->num9)[i] = (vec2->arr)[i] = (vec2->arr1)[i] = (vec2->arr2)[i] = (vec2->arr3)[i] = (vec2->arr4)[i] = (vec2->arr5)[i] = (vec2->arr6)[i] = (vec2->arr7)[i] = (vec2->arr8)[i] = (vec2->arr9)[i] = (vec2->var)[i] = (vec2->var1)[i] = (vec2->var2)[i] = (vec2->var3)[i] = (vec2->var4)[i] = (vec2->var5)[i] = (vec2->var6)[i] = (vec2->var7)[i] = fact((vec2->number)[i]%3);
+    (vec2->var8)[i] = i%2;
+  }
+  vec2->enum1[0] = vec2->enum1[3] = vec2->enum1[6] = vec2->enum1[9] = GREY;
+  vec2->enum1[1] = vec2->enum1[4] = vec2->enum1[7] = DARK;
+  vec2->enum1[2] = vec2->enum1[5] = vec2->enum1[8] = WHITE;
+  return 0;
+}
---
+++ testcases/array/src/array_test.c
@@ -0,0 +1,319 @@
+/*==============================================================================
+  Copyright (c) 2012-2014,2017,2020 Qualcomm Technologies, Inc.
+  All rights reserved. Qualcomm Proprietary and Confidential.
+==============================================================================*/
+
+#include "AEEStdErr.h"
+#include "array.h"
+#include "array_test.h"
+#include "rpcmem.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "remote.h"
+#include "unistd.h"
+
+#pragma weak remote_session_control
+#ifdef __hexagon__
+#define sleep(x) {/* Do nothing for simulator */}
+#endif
+
+int local_array_sum(alpha* vec, int64_t* res)
+{
+  printf("reached local execution\n");
+  int sum=0;
+  for(int i=0;i<10;i++)
+  {
+      sum+=(vec->number)[i];
+  }
+
+  *res = sum;
+  return 0;
+}
+
+int array_test(int domain, int num, bool is_signedpd_requested) {
+
+  int nErr = AEE_SUCCESS;
+  alpha* test = NULL;
+  alpha *vec1 = NULL;
+  int64_t* res = NULL;
+  int  len = 0, resLen = 29;
+  int retry = 10, seqLen = 2, sum1 = 0, sum2 = 0;
+  remote_handle64 handleSum = -1;
+  char *uri = NULL;
+  num = 15;
+  beta *struc1 = NULL, *struc2 = NULL, *seq3 = NULL, *seq4 = NULL;
+  alpha *seq1 = NULL, *seq2 = NULL;
+  len = sizeof(*test) * 1;
+
+  int heapid = RPCMEM_HEAP_ID_SYSTEM;
+#if defined(SLPI) || defined(MDSP)
+  heapid = RPCMEM_HEAP_ID_CONTIG;
+#endif
+
+  if (0 == (test = (alpha *)rpcmem_alloc(heapid, RPCMEM_DEFAULT_FLAGS, len))) {
+    nErr = AEE_ENORPCMEMORY;
+    printf("ERROR 0x%x: memory alloc failed\n", nErr);
+    goto bail;
+  }
+  if (0 == (res = (int64_t*)rpcmem_alloc(heapid, RPCMEM_DEFAULT_FLAGS, sizeof(int64_t)*resLen))) {
+    nErr = AEE_ENORPCMEMORY;
+    printf("ERROR 0x%x: memory alloc failed\n", nErr);
+    goto bail;
+  }
+  //Allocating memory to sum2 rout param
+  if (0 == (vec1 = (alpha*)rpcmem_alloc(heapid, RPCMEM_DEFAULT_FLAGS, sizeof(alpha)))) {
+    nErr = AEE_ENORPCMEMORY;
+    printf("ERROR 0x%x: memory alloc failed\n", nErr);
+    goto bail;
+  }
+  //Allocating memory for method1 params
+  if (0 == (struc1 = (beta*)rpcmem_alloc(heapid, RPCMEM_DEFAULT_FLAGS, sizeof(beta)))) {
+    nErr = AEE_ENORPCMEMORY;
+    printf("ERROR 0x%x: memory alloc failed\n", nErr);
+    goto bail;
+  }
+  if (0 == (struc2 = (beta*)rpcmem_alloc(heapid, RPCMEM_DEFAULT_FLAGS, sizeof(beta)))) {
+    nErr = AEE_ENORPCMEMORY;
+    printf("ERROR 0x%x: memory alloc failed\n", nErr);
+    goto bail;
+  }
+  // Allocating memory for method2 params
+  if (0 == (seq1 = (alpha*)rpcmem_alloc(heapid, RPCMEM_DEFAULT_FLAGS, sizeof(alpha)*seqLen))) {
+    nErr = AEE_ENORPCMEMORY;
+    printf("ERROR 0x%x: memory alloc failed\n", nErr);
+    goto bail;
+  }
+  if (0 == (seq2 = (alpha*)rpcmem_alloc(heapid, RPCMEM_DEFAULT_FLAGS, sizeof(alpha)*seqLen))) {
+    nErr = AEE_ENORPCMEMORY;
+    printf("ERROR 0x%x: memory alloc failed\n", nErr);
+    goto bail;
+  }
+  //Allocating memory for method3 params
+  if (0 == (seq3 = (beta*)rpcmem_alloc(heapid, RPCMEM_DEFAULT_FLAGS, sizeof(beta)*seqLen))) {
+    nErr = AEE_ENORPCMEMORY;
+    printf("ERROR 0x%x: memory alloc failed\n", nErr);
+    goto bail;
+  }
+  if (0 == (seq4 = (beta*)rpcmem_alloc(heapid, RPCMEM_DEFAULT_FLAGS, sizeof(beta)*seqLen))) {
+    nErr = AEE_ENORPCMEMORY;
+    printf("ERROR 0x%x: memory alloc failed\n", nErr);
+    goto bail;
+  }
+  printf("- Initializing in params \n");
+  //Initializing sum method in params
+  for(int i=0;i<10;i++)
+  {
+    (test->num2)[i] =(wchar_t) i;
+    (test->num3)[i] =(unsigned char) i;
+    (test->num4)[i] = (char) i;
+    (test->number)[i] = (test->num5)[i] = (test->num6)[i] = (test->num7)[i]= (test->num8)[i] = (test->num9)[i] = (test->arr)[i] = (test->arr1)[i] = (test->arr2)[i] = (test->arr3)[i] = (test->arr4)[i] = (test->arr5)[i] = (test->arr6)[i] = (test->arr7)[i] = (test->arr8)[i] = (test->arr9)[i] = (test->var)[i] = (test->var1)[i] = (test->var2)[i] = (test->var3)[i] = (test->var4)[i] = (test->var5)[i] = (test->var6)[i] = (test->var7)[i] = i;
+    (test->var8)[i] = i%2;
+  }
+  test->enum1[0] = test->enum1[3] = test->enum1[6] = test->enum1[9] = DARK;
+  test->enum1[1] = test->enum1[4] = test->enum1[7] = GREY;
+  test->enum1[2] = test->enum1[5] = test->enum1[8] = WHITE;
+
+  //Initializing method1 in params
+  for(int i=0;i<5;i++){
+    struc1->a.num[i] = i;
+  }
+  //Initializing method2 in params
+  for(int j=0;j<seqLen;j++){
+    for(int i=0;i<10;i++){
+    (seq1[j].num2)[i] =(wchar_t) (i+j);
+    (seq1[j].num3)[i] =(unsigned char) (i+j);
+    (seq1[j].num4)[i] = (char) (i+j);
+    (seq1[j].number)[i] = (seq1[j].num5)[i] = (seq1[j].num6)[i] = (seq1[j].num7)[i]= (seq1[j].num8)[i] = (seq1[j].num9)[i] = (seq1[j].arr)[i] = (seq1[j].arr1)[i] = (seq1[j].arr2)[i] = (seq1[j].arr3)[i] = (seq1[j].arr4)[i] = (seq1[j].arr5)[i] = (seq1[j].arr6)[i] = (seq1[j].arr7)[i] = (seq1[j].arr8)[i] = (seq1[j].arr9)[i] = (seq1[j].var)[i] = (seq1[j].var1)[i] = (seq1[j].var2)[i] = (seq1[j].var3)[i] = (seq1[j].var4)[i] = (seq1[j].var5)[i] = (seq1[j].var6)[i] = (seq1[j].var7)[i] = (i+j);
+    (seq1[j].var8)[i] = (i+j)%2;
+    seq1[j].enum1[i] = WHITE;
+    }
+  }
+  //Initializing method3 in params
+  for(int i=0;i<seqLen;i++){
+    for(int j=0;j<5;j++){
+      seq3[i].a.num[j] = i+j;
+    }
+  }
+
+    printf("- compute sum on domain %d\n", domain);
+
+    if (domain == ADSP_DOMAIN_ID)
+      uri = array_URI ADSP_DOMAIN;
+    else if (domain == CDSP_DOMAIN_ID)
+      uri = array_URI CDSP_DOMAIN;
+    else if (domain == MDSP_DOMAIN_ID)
+      uri = array_URI MDSP_DOMAIN;
+    else if (domain == SDSP_DOMAIN_ID)
+      uri = array_URI SDSP_DOMAIN;
+    else {
+      nErr = AEE_EINVALIDDOMAIN;
+      printf("ERROR 0x%x: unsupported domain %d\n", nErr, domain);
+      goto bail;
+    }
+
+      if(remote_session_control) {
+        struct remote_rpc_control_unsigned_module data;
+        data.domain = domain;
+        if (is_signedpd_requested)
+          data.enable = 0;
+        else
+          data.enable = 1;
+        if (AEE_SUCCESS != (nErr = remote_session_control(DSPRPC_CONTROL_UNSIGNED_MODULE, (void*)&data, sizeof(data)))) {
+          printf("ERROR 0x%x: remote_session_control failed for CDSP\n", nErr);
+          goto bail;
+        }
+      } else {
+        nErr = AEE_EUNSUPPORTED;
+        printf("ERROR 0x%x: remote_session_control interface is not supported on this device\n", nErr);
+        goto bail;
+      }
+
+    do {
+      if (AEE_SUCCESS == (nErr = array_open(uri, &handleSum))) {
+        printf("\nCall array_sum on the DSP\n");
+        nErr = array_sum(handleSum, test, res, resLen);
+      }
+    int k=0;
+      if (!nErr) {
+        for(int i=0;i<resLen-2;i++){
+          if(res[i]!=45){
+            k++;
+            break;
+          }
+        }
+        if(res[resLen-2] != 5 || res[resLen-1] != 9)
+          k++;
+        if(!k)
+          printf("Completion of array_sum on the DSP\n");
+        else
+          printf("=== Data Mismatch ===\n");
+        break;
+      } else {
+        if (nErr == AEE_ECONNRESET) {
+          /* AEE_ECONNRESET is returned when Sub-system restart (SSR) happens */
+          retry--;
+          sleep(5);
+        } else if (nErr == AEE_ENOSUCH || nErr == AEE_EBADSTATE) {
+          /* AEE_ENOSUCH is returned when Protection domain restart (PDR) happens and
+          AEE_EBADSTATE is returned when PD is exiting or crashing.*/
+          retry -= 2;
+        } else {
+          break;
+        }
+      }
+
+      /* Close the handle and retry handle open */
+      if (handleSum != -1) {
+        if (AEE_SUCCESS != (nErr = array_close(handleSum))) {
+          printf("ERROR 0x%x: Failed to close handle\n", nErr);
+        }
+      }
+    } while(retry);
+
+    if (nErr) {
+      printf("- retry attempt unsuccessful. Timing out....\n");
+      printf("ERROR 0x%x: Failed to compute sum on domain %d\n", nErr, domain);
+    }
+
+    printf("\nCall array_sum2 on the DSP\n");
+    if (AEE_SUCCESS == (nErr = array_sum2(handleSum, vec1, test))) {
+      sum1 = 0, sum2 = 0;
+      for(int i=0;i<10;i++){
+        sum1 = sum1 + (vec1->number)[i] + (int)(vec1->num2)[i] + (int)(vec1->num3)[i] + (int)(vec1->num4)[i] + (vec1->num5)[i] + (vec1->num6)[i] + (vec1->num7)[i] + (vec1->num8)[i] + (vec1->num9)[i] + (vec1->arr)[i] + (vec1->arr1)[i] + (vec1->arr2)[i] + (vec1->arr3)[i] + (vec1->arr4)[i] + (vec1->arr5)[i] + (vec1->arr6)[i] + (vec1->arr7)[i] + (vec1->arr8)[i] + (vec1->arr9)[i] + (vec1->var)[i] + (vec1->var1)[i] + (vec1->var2)[i] + (vec1->var3)[i] + (vec1->var4)[i] + (vec1->var5)[i] + (vec1->var6)[i] + (vec1->var7)[i] + (vec1->var8)[i] + (vec1->enum1)[i];
+
+        sum2 = sum2 + (test->number)[i] + (int)(test->num2)[i] + (int)(test->num3)[i] + (int)(test->num4)[i] + (test->num5)[i] + (test->num6)[i] + (test->num7)[i] + (test->num8)[i] + (test->num9)[i] + (test->arr)[i] + (test->arr1)[i] + (test->arr2)[i] + (test->arr3)[i] + (test->arr4)[i] + (test->arr5)[i] + (test->arr6)[i] + (test->arr7)[i] + (test->arr8)[i] + (test->arr9)[i] + (test->var)[i] + (test->var1)[i] + (test->var2)[i] + (test->var3)[i] + (test->var4)[i] + (test->var5)[i] + (test->var6)[i] + (test->var7)[i] + (test->var8)[i] + (test->enum1)[i];
+      }
+      if(sum1 != 463 || sum2 != 502)
+        printf("=== Data Mismatch ===\n");
+      else
+        printf("Completion of array_sum2 on DSP\n");
+    }
+    if (nErr) {
+      printf("ERROR 0x%x: Failed to run array_method1 on domain %d\n", nErr, domain);
+    }
+    printf("\nCall array_method1 on the DSP\n");
+    if (AEE_SUCCESS == (nErr = array_method1(handleSum, struc1, struc2, struc1))) {
+      int sum1 = 0;
+      for(int j=0;j<5;j++)
+          sum1 = sum1 + struc1->a.num[j];
+      if(struc2->a.num[0] != 1 || struc2->a.num[1] != 1 || struc2->a.num[2] != 2 || struc2->a.num[3] != 6 || struc2->a.num[4] != 24 || sum1 != 30)
+        printf("=== Data Mismatch ===\n");
+      else
+        printf("Completion of array_method1 on DSP\n");
+    }
+    if (nErr) {
+      printf("ERROR 0x%x: Failed to run array_method1 on domain %d\n", nErr, domain);
+    }
+    printf("\nCall array_method2 on the DSP\n");
+    if (AEE_SUCCESS == (nErr = array_method2(handleSum, seq1, seqLen, seq2, seqLen, seq1, seqLen))) {
+      sum1 = 0, sum2 =0;
+      for(int i=0;i<seqLen;i++){
+        for(int j=0;j<10;j++){
+          sum1 = sum1 +seq1[i].number[j] + (int)seq1[i].num2[j] + (int)seq1[i].num3[j] + (int)seq1[i].num4[j] + seq1[i].num5[j] + seq1[i].num6[j] + seq1[i].num7[j] + seq1[i].num8[j] + seq1[i].num9[j] + seq1[i].arr[j] + seq1[i].arr1[j] + seq1[i].arr2[j] + seq1[i].arr3[j] + seq1[i].arr4[j]+ seq1[i].arr5[j]+ seq1[i].arr6[j]+ seq1[i].arr7[j]+ seq1[i].arr8[j]+ seq1[i].arr9[j] + seq1[i].var[j]+ seq1[i].var1[j]+ seq1[i].var2[j]+ seq1[i].var3[j]+ seq1[i].var4[j]+ seq1[i].var5[j]+ seq1[i].var6[j]+ seq1[i].var7[j]+ seq1[i].var8[j] + seq1[i].enum1[j];
+          sum2 = sum2 +seq2[i].number[j] + (int)seq2[i].num2[j] + (int)seq2[i].num3[j] + (int)seq2[i].num4[j] + seq2[i].num5[j] + seq2[i].num6[j] + seq2[i].num7[j] + seq2[i].num8[j] + seq2[i].num9[j] + seq2[i].arr[j] + seq2[i].arr1[j] + seq2[i].arr2[j] + seq2[i].arr3[j] + seq2[i].arr4[j]+ seq2[i].arr5[j]+ seq2[i].arr6[j]+ seq2[i].arr7[j]+ seq2[i].arr8[j]+ seq2[i].arr9[j] + seq2[i].var[j]+ seq2[i].var1[j]+ seq2[i].var2[j]+ seq2[i].var3[j]+ seq2[i].var4[j]+ seq2[i].var5[j]+ seq2[i].var6[j]+ seq2[i].var7[j]+ seq2[i].var8[j] + seq2[i].enum1[j];
+        }
+      }
+      if(sum2 != 954 || sum1!= 934)
+        printf("=== Data Mismatch %d, %d===\n",sum1,sum2);
+      else
+        printf("Completion of array_method2 on DSP\n");
+    }
+    if (nErr) {
+      printf("ERROR 0x%x: Failed to run array_method2 on domain %d\n", nErr, domain);
+    }
+    printf("\nCall array_method3 on the DSP\n");
+    if (AEE_SUCCESS == (nErr = array_method3(handleSum, seq3, seqLen, seq4, seqLen, seq3, seqLen))) {
+      sum1 = 0, sum2 = 0;
+      for(int i=0;i<seqLen;i++){
+        for(int j=0;j<5;j++){
+          sum1 = sum1 + seq3[i].a.num[j];
+          sum2 = sum2 + seq4[i].a.num[j];
+        }
+      }
+      if(sum1 != 187|| sum2 != 85)
+        printf("=== Data Mismatch %d, %d===\n", sum1, sum2);
+      else
+        printf("Completion of array_method3 on DSP\n");
+    }
+    if (nErr) {
+      printf("ERROR 0x%x: Failed to run array_method3 on domain %d\n", nErr, domain);
+    }
+    if (handleSum != -1) {
+      if (AEE_SUCCESS != (nErr = array_close(handleSum))) {
+        printf("ERROR 0x%x: Failed to close handleSum\n", nErr);
+      }
+    }
+
+bail:
+  if (test) {
+    rpcmem_free(test);
+  }
+  if (res) {
+    rpcmem_free(res);
+  }
+  if (struc1) {
+    rpcmem_free(struc1);
+  }
+  if (struc2) {
+    rpcmem_free(struc1);
+  }
+  if (seq1) {
+    rpcmem_free(seq1);
+  }
+  if (seq2) {
+    rpcmem_free(seq2);
+  }
+  if (seq3) {
+    rpcmem_free(seq3);
+  }
+  if (seq4) {
+    rpcmem_free(seq4);
+  }
+  if (vec1) {
+    rpcmem_free(vec1);
+  }
+  return nErr;
+}
